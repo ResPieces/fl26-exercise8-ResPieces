e313b3e73aa0b441fe8ffa444ebe4481a23bd374
Res Sharpe <resmakesthings@gmail.com>
2026-09-16 23:04:23 -0400

    Submission!

---
+++ label_printer.h
@@ -0,0 +1,15 @@
+#ifndef LABEL_PRINTER_H
+#define LABEL_PRINTER_H
+
+#include <QLabel>
+
+class LabelPrinter : public QLabel
+{
+    Q_OBJECT
+public:
+    LabelPrinter(const char *labelstr);
+
+    void keyPressEvent(QKeyEvent *ev);
+};
+
+#endif
